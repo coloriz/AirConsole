0e846061c0a7df199c41aedcbbefa762defba8fc
deathwish <deathwish256@yahoo.com>
2017-07-28 17:21:38 +0900

    node에서 import 가능하도록 AirConsole 모듈화

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
 const Controller = require('./controller');
 
-async function eunju_test() {
-
-    let eunju = new Controller({
-        name: '은주관',
-        ip: '210.110.36.32',
-        firmware: '1.4.4.11',
-        sessionId: 'samsung.http.server0.1582141870650774'
-    })
-
-    let status = await eunju.readAll().then(v => v).catch(e => {throw e});
-    console.log(status);
-
-    eunju.update('00.00.00', {power: 'on', temperature: 23, fanSpeed: 'high', airSwingUD: 'false', airSwingLR: 'false', operationMode: 'cool'})
-}
-
-async function bukak_test() {
-
-    let bukak = new Controller({
-        name: '북악관',
-        ip: '210.110.32.20',
-        firmware: '1.4.3',
-        sessionId: null
-    })
-
-    let status = await bukak.readAll().then(v => v).catch(e => {throw e});
-    console.log(status);
-
-    bukak.update('00.00.00', {power: 'on', temperature: 18, fanSpeed: 'high', airSwingUD: 'true', airSwingLR: 'false', operationMode: 'cool'})
-}
-
-eunju_test()
-bukak_test()
+module.exports = Controller;
